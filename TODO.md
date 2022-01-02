813510d11dd0c19b3cba8c9a2f3f7ce1a030b507
Ryan Jeffares <ryandjeffares@gmail.com>
2022-01-02 16:03:26 +0000

    added grammar files

---
+++ TODO.md
@@ -0,0 +1,12 @@
+# TODO
+* `switch`
+* `break`
+* `continue`
+* Integer types
+  * `%`
+  * Bitwise operators
+* Imports
+* Add-assign operators
+* Lists and Hash Tables for users
+* Optimisations
+* Allow more than 256 values in scope
--- src/common.h
+++ src/common.h
@@ -5,8 +5,14 @@
 #include <stddef.h>
 #include <stdint.h>
 
+#if defined(_DEBUG) || !defined(NDEBUG) || defined(DEBUG)
+    #define CLOX_DEBUG
+#endif
+
+#ifdef CLOX_DEBUG
 #define DEBUG_PRINT_CODE
 #define DEBUG_TRACE_EXECUTION
+#endif
 
 #define UINT8_COUNT (UINT8_MAX + 1)
 
